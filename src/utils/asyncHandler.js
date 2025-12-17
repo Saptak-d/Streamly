35888c6799119d60a0e4bb656f5afbaa2b92944a
saptak dutta <duttasaptak79@gmail.com>
2025-12-18 01:36:40 +0530

    create asyncHandler wrapper function to avoid the repetition of Try-catch block

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,36 @@
+const  asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err)=>next(err))
+    }
+}
+
+
+
+
+export { asyncHandler}
+
+
+
+//const asyncHandler  = () => {}
+//const asyncHandler = (func) => () => {}
+// const asyncHandler = (fnc) => async () => {}
+
+//  const asyncHandler = (fnc) => async () => {
+    
+
+//    try {
+
+//     await fnc(req,res,next)
+    
+//    } catch (error) {
+//       res.status(err.code || 500).json({
+//         success : false,
+//         message : err.message
+//       })
+//    }
+
+
+
+
+
