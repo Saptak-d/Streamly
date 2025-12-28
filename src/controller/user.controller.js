322fd7d876a341ffd7b15921306f410c064fbff7
saptak dutta <duttasaptak79@gmail.com>
2025-12-28 07:57:30 +0530

    create the updateUserCoverImage controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -313,9 +313,9 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
 })
 
-const updateAvatar = asyncHandler(async(req,res)=>{
+const updateUserAvatar = asyncHandler(async(req,res)=>{
    
-   const avatarLocalPath = req.files?.path;
+   const avatarLocalPath = req.file?.path;
 
    if(!avatarLocalPath){
       throw new ApiError(400,"The new Avatar is Required ")
@@ -351,7 +351,34 @@ const updateAvatar = asyncHandler(async(req,res)=>{
       )
 })
 
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+  
+  const coverImageLocalPath = req.file?.path
 
+  if(!coverImageLocalPath){
+     throw new ApiError(401,"The coverImage is required ");
+  } 
+   
+    const coverImage =  await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImage.url){
+      throw new ApiError(400,"Error while uploading coverImage ")
+    }
+
+    const user  = await User.findByIdAndUpdate(
+       req.user?._id,
+       {
+        coverImage : coverImage.url
+       },
+       {new : true},
+    ).select("-password")
+    
+    return res
+    .status(200)
+    .json(
+      new ApiResponse(200,user,"The coverImage is Successfully Changed ")
+    )
+})
 
 
 
@@ -365,5 +392,6 @@ refreshAccessToken,
 changeCurrentPassword,
 getCurrentUser,
 updateAccountDetails,
-updateAvatar
+updateUserAvatar,
+updateUserCoverImage
 }
