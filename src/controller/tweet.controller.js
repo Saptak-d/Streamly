476645552b4afc89e71ba37f9403dcf8a4d61434
saptak dutta <duttasaptak79@gmail.com>
2026-01-12 23:32:51 +0530

    create the deleteTweet controller of tweet controllers and set up it's route and all

--- src/controller/tweet.controller.js
+++ src/controller/tweet.controller.js
@@ -1,15 +1,16 @@
-import { Tweet } from "../models/twite.models.js";
+import { Tweet } from "../models/tweet.models.js"
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiErrors.js";
 
 
 const createTweet = asyncHandler(async(req , res)=>{
+
     const {content} = req.body
     if(!content){
         throw new ApiError(404,"content is required")
     }
-    const user = req.user._id;
+    const user = req.user?._id;
      if(!user){
         throw new ApiError(404,"the user is need to login  ")
      }
@@ -26,8 +27,7 @@ const createTweet = asyncHandler(async(req , res)=>{
       .json(
         new ApiResponse(200,tweet,"the tweet is created successfully")
       )
-}
-)
+})
 const getUserTweets = asyncHandler(async(req,res)=>{
 
      const {userId}  = req.params
@@ -48,11 +48,82 @@ const getUserTweets = asyncHandler(async(req,res)=>{
         new ApiResponse(200,tweets,"all Tweets successfully returned")
       )
 
+}) 
+const updateTweet  = asyncHandler(async(req,res)=>{
+   
+      const {content} = req.body
+
+       if(!content){
+         throw new ApiError(404,"the content is required")
+       }
+
+      const {tweetId}  = req.params
+      if(!tweetId){
+         throw new ApiError(404,"tweetId is required")
+      }
+       
+      const tweet = await Tweet.findById(tweetId)
+
+       if(!tweet){
+        throw new ApiError(404,"there is no Tweet exist in this Id ")
+       }
+      
+      const userId = req.user._id
+
+      if(!userId){
+        throw new ApiError(404,"user invalid")
+      }
+
+      if(tweet.owner.toString() !== userId.toString()){
+        throw new ApiError(404,"user invalid to Edit")
+      }
+
+       tweet.content = content;
+
+ 
+        await tweet.save();
+
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(200,tweet,"the tweet is successfully changed")
+      )
 })
+const deleteTweet = asyncHandler(async(req,res)=>{
+    const {tweetId} = req.params;
+    const userId = req.user._id;
+    if(!tweetId || !userId){
+        throw new ApiError(404,"the tweet ID and userId is required")
+    }
+
+    const tweet = await Tweet.findById(tweetId)
+
+    if(!tweet){
+        throw new ApiError(404,"no Tweet is found for this tweet ID")
+    }
+
+    if(tweet.owner.toString() !== userId.toString()){
+        throw new ApiError(400,"The tweet is not belongs to the user ")
+    }
+
+     const deletedTweet = await tweet.deleteOne()
+
+     if(!deletedTweet){
+        throw new ApiError(500,"internal server error while Deleting the Tweet ")
+     }
+
+     return res
+     .status(200)
+     .json(
+        new ApiResponse(200,deletedTweet,"the Tweet is Deleted")
+     )
+})
+
 
 
 export {
     createTweet,
-    getUserTweets
-
+    getUserTweets,
+    updateTweet,
+    deleteTweet
 }
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {createTweet, getUserTweets}  from "../controller/tweet.controller.js"
+import {createTweet, deleteTweet, getUserTweets, updateTweet}  from "../controller/tweet.controller.js"
 import { verifyJwt } from "../middleware/auth.middleware.js";
 
 
@@ -7,6 +7,8 @@ const router = Router()
 
 router.route("/createTweet").post(verifyJwt,createTweet)
 router.route("/getUserTweets/:userId").get(verifyJwt,getUserTweets)
+router.route("/updateTweet/:tweetId").patch(verifyJwt,updateTweet)
+router.route("/deleteTweet/:tweetId").delete(verifyJwt,deleteTweet)
 
 
 export default router;
