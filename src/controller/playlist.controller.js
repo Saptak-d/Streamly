915ee1f5aae6409b465d986ce195a85c5742c910
saptak dutta <duttasaptak79@gmail.com>
2026-01-15 21:59:45 +0530

    create the getUserPlayList controller

--- src/controller/playlist.controller.js
+++ src/controller/playlist.controller.js
@@ -2,6 +2,7 @@ import { Playlist } from "../models/playlist.model.js";
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiErrors.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
+import mongoose from "mongoose";
 
 const createPlaylist  = asyncHandler(async(req ,res)=>{
     const {name , description } = req.body
@@ -30,4 +31,64 @@ const createPlaylist  = asyncHandler(async(req ,res)=>{
      .json(
         new ApiResponse(200,newPlayList,"the new playlist is created successfully")
      )
+})
+
+const getUserPlaylists = asyncHandler(async(req,res)=>{
+    const{userId} = req.params
+    if(!userId){
+        throw new ApiError(400,"the user iD is required")
+    }
+    const playlist =  await Playlist.aggregate([
+        {$match: {owner : new mongoose.Types.ObjectId(userId)}},
+        {
+            $lookup :{
+                from : "videos",
+                localField : "videos",
+                foreignField : "_id",
+                 as : "video",
+                 pipeline : [
+                    {
+                        $lookup :{
+                            from : "users",
+                            localField : "owner",
+                            foreignField : "_id",
+                             as : "owner"
+                        },
+                    },
+                     {$unwind : "$owner"},
+                    {
+                      $project: {
+                            _id: 1,
+                             title: 1,
+                            description: 1,
+                            videoFile: 1,
+                            thumbnail: 1,
+                            owner: {
+                             _id: "$owner._id",
+                             username: "$owner.username",
+                             fullName: "$owner.fullName",
+                             avatar: "$owner.avatar",
+                          },
+                      }
+                    }
+                 ]
+            }
+        },
+        {
+            $project :{
+                   _id: 1,
+                   name: 1,
+                   description: 1,
+                   videos: 1,
+                   createdAt: 1,
+            }
+        }
+    ])
+   return res.status(200).json(
+    new ApiResponse(
+      200,
+      playlists,
+      "User playlists fetched successfully"
+    )
+  );
 })
