e7150cd76cab2271683a30837fc668773b8c03e9
saptak dutta <duttasaptak79@gmail.com>
2026-01-17 23:48:30 +0530

    set up router for updatePlaylist

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import {verifyJwt} from "../middleware/auth.middleware.js"
-import {createPlaylist,addVideosToPlaylist,getUserPlaylists,getPlaylistById} from "../controller/playlist.controller.js"
+import {createPlaylist,addVideosToPlaylist,getUserPlaylists,getPlaylistById,removeVideoFromPlaylist} from "../controller/playlist.controller.js"
 
 const router  = Router()
 
 router.route("/createPlaylist").post(verifyJwt,createPlaylist);
 router.route("/addVideosToPlaylist/:playlistId/:videoId").post(verifyJwt,addVideosToPlaylist)
 router.route("/getUserPlaylists/:userId").get(verifyJwt,getUserPlaylists)
-router.route("/getPlaylistById/:playlistId").get(verifyJwt,getPlaylistById )
+router.route("/getPlaylistById/:playlistId").get(verifyJwt,getPlaylistById)
+router.route("/updatedPlaylist/:playlistId/:videoId").patch(verifyJwt,removeVideoFromPlaylist)
 
 export default router
