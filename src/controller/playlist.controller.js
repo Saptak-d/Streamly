cd659a20a534782fcc8e84d4048647a0cf94fd8b
saptak dutta <duttasaptak79@gmail.com>
2026-01-17 20:34:56 +0530

    setup the route of getplaylistById controller

--- src/controller/playlist.controller.js
+++ src/controller/playlist.controller.js
@@ -173,6 +173,7 @@ export {
     createPlaylist,
     addVideosToPlaylist,
     getUserPlaylists,
+    getPlaylistById,
 
 
 }
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import {verifyJwt} from "../middleware/auth.middleware.js"
-import {createPlaylist,addVideosToPlaylist,getUserPlaylists} from "../controller/playlist.controller.js"
+import {createPlaylist,addVideosToPlaylist,getUserPlaylists,getPlaylistById} from "../controller/playlist.controller.js"
 
 const router  = Router()
 
 router.route("/createPlaylist").post(verifyJwt,createPlaylist);
 router.route("/addVideosToPlaylist/:playlistId/:videoId").post(verifyJwt,addVideosToPlaylist)
 router.route("/getUserPlaylists/:userId").get(verifyJwt,getUserPlaylists)
+router.route("/getPlaylistById/:playlistId").post(verifyJwt,getPlaylistById )
 
 export default router
