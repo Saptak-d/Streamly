9164ed71490961db853d48a1123c8ac0468ce05e
saptak dutta <duttasaptak79@gmail.com>
2026-01-22 03:26:03 +0530

    setup the route for the getlikedVIdeo controller route

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import {verifyJwt} from "../middleware/auth.middleware.js"
-import { toggleVideoLike,toggleCommentLike,toggleTweetLike } from "../controller/like.controller.js";
+import { toggleVideoLike,toggleCommentLike,toggleTweetLike,getLikedVideos } from "../controller/like.controller.js";
 
 const router  = Router()
 
 router.route("/toggleVideoLike/:videoId").get(verifyJwt,toggleVideoLike)
 router.route("/toggleCommentLike/:commentId").get(verifyJwt,toggleCommentLike)
 router.route("/toggleTweetLike/:tweetId").get(verifyJwt,toggleTweetLike)
+router.route("/getLikedVideos").get(verifyJwt,getLikedVideos)
 
 
 export default router;
