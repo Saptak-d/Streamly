4840e5f2f30707370938ecdaf2eee00dda6cbfaf
saptak dutta <duttasaptak79@gmail.com>
2026-02-02 04:18:48 +0530

    install nodemailer and mailgen for seanding email and crafting emails for differet routes and setup a reusable mail seanding function and also craft 2 desings of mail body one is for verify email  and change password

---
+++ src/utils/mail.js
@@ -0,0 +1,94 @@
+import { body } from "express-validator";
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer"
+
+const sendMail = async (options)=>{
+
+    // Configure mailgen
+     const mailGenerator = new Mailgen({
+        theme: 'default',
+        product: {
+        name: 'Streamly',
+        link: 'https://mailgen.js/'
+       }
+   });
+
+
+   const emailText = mailGenerator.generatePlaintext(options.mailGenContent);
+   const emailBody  = mailGenerator.generate(options.mailGenContent)// Generate an HTML email with the provided contents
+
+   // Create a transporter using Ethereal test credentials.
+ // For production, replace with your actual SMTP server details.
+  const transporter = nodemailer.createTransport({
+   host: process.env.MAILTRAP_SMTP_HOST,
+   port: process.env.MAILTRAP_SMTP_PORT,
+   secure: false, // Use true for port 465, false for port 587
+   auth: {
+     user: process.env.MAILTRAP_SMTP_USER,
+      pass:  process.env.MAILTRAP_SMTP_PASS
+   },
+});
+
+ const mail = {
+    from: 'maddison53@ethereal.email',//sender address
+    to: options.email,
+    subject: options.subject,
+    text: emailText, // Plain-text version of the message
+    html: emailBody,
+ }
+
+  try {
+    
+    await transporter.sendMail(mail)
+
+  } catch (error) {
+     console.error("Emailed failed ")
+  }
+
+}
+
+const  emailVerificationMailGenContent = (username,verificationUrl)=>{
+    return {
+        body : {
+        name: username,
+        intro: 'Welcome to Streamly',
+        action: {
+            instructions: 'To get started with Streamly , please click here:',
+            button: {
+                color: '#22BC66', // Optional action button color
+                text: 'Confirm your account',
+                link: verificationUrl,
+            }   
+        },
+        outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+        }
+    }
+}
+
+const  forgotPasswordMailGenCOntent = (username,passwordResetUrl)=>{
+    return {
+        body : {
+        name: username,
+        intro: 'to change the password click the buttonr',
+        action: {
+            instructions: 'To get started with Streamly , please click here:',
+            button: {
+                color: '#4f94f4ff', // Optional action button color
+                text: 'Reset Password',
+                link: passwordResetUrl,
+            }   
+        },
+        outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+        }
+    }
+}
+
+
+
+//structure of calling the function write it for late  
+// sendMail({
+//     email : user.email,
+//     subject : "aaa",
+//     mailGenContent : emailVerificationMailGenContent(username, ` url `)
+
+// })
